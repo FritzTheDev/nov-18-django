c20e87d94fdad23e2b3555206657335f0ab33742
Frederick Johnson <fritzj96@gmail.com>
2019-11-19 10:13:11 -0800

    Made branch uuid the primary key

--- bank/models/branch.py
+++ bank/models/branch.py
@@ -2,5 +2,5 @@ from django.db.models import Model, CharField
 from uuid import uuid4
 
 class Branch(Model):
-    branch_uuid = CharField(default=uuid4(), max_length=36)
+    branch_uuid = CharField(default=uuid4(), max_length=36, primary_key=True)
     branch_address = CharField(max_length=100)
