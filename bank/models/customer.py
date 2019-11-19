d4ee7c95c8e7d677bcd19cdca21e3aa2b560d17a
Frederick Johnson <fritzj96@gmail.com>
2019-11-19 09:59:06 -0800

    added DateTimeField for customer creation

--- bank/models/customer.py
+++ bank/models/customer.py
@@ -1,6 +1,8 @@
-from django.db.models import Model, OneToOneField, CharField, ForeignKey, CASCADE
+from django.db.models import Model, OneToOneField, CharField, DateTimeField, ForeignKey, CASCADE
 
 class Customer(Model):
     base_user = OneToOneField('auth.User', on_delete=CASCADE, primary_key=True)
+    customer_since = DateTimeField(auto_now_add=True)
     first_name = CharField(max_length=15)
     last_name = CharField(max_length=20)
+    
