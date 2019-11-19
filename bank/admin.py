f613b4ac561fbce1f9d59a8ad3f5fb9d2c153275
Frederick Johnson <fritzj96@gmail.com>
2019-11-19 10:22:09 -0800

    registered models in the admin

--- bank/admin.py
+++ bank/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models.account import Account
+from .models.customer import Customer
+from .models.product import Product
+from .models.branch import Branch
+
 # Register your models here.
+
+admin.site.register(Account)
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Branch)
