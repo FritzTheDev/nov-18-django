dba3514ff65c1fa0eeab61316c091ee40cffb28c
Frederick Johnson <fritzj96@gmail.com>
2019-11-19 10:33:40 -0800

    added a selfString to account model

--- bank/models/account.py
+++ bank/models/account.py
@@ -8,6 +8,9 @@ account_type_choices = (
 
 class Account(Model):
     owner = ForeignKey('bank.customer', on_delete=CASCADE, related_name='accounts')
-    account_name = CharField(max_length=255)
+    account_name = CharField(max_length=25)
     account_type = CharField(max_length=3, choices=account_type_choices)
     created_on = DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.account_name
