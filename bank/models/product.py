8bc02eaa4de7e503c4aab0e2e4bc03cecbc9d183
Frederick Johnson <fritzj96@gmail.com>
2019-11-19 10:35:50 -0800

    added selfstring to product

--- bank/models/product.py
+++ bank/models/product.py
@@ -9,3 +9,11 @@ product_type_choices = (
 class Product(Model):
     product_type = CharField(max_length=3, choices=product_type_choices)
     product_description = CharField(max_length=1000)
+
+    def __str__(self):
+        if self.product_type == 'crd':
+            return 'Credit'
+        if self.product_type == 'chk':
+            return 'Checking'
+        if self.product_type == 'crd':
+            return 'Savings'
