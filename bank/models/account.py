59f3b6cac2ed6634233df062398f1cedf87a0f3c
Frederick Johnson <fritzj96@gmail.com>
2019-11-19 10:32:32 -0800

    fixed string reference in account.py

--- bank/models/account.py
+++ bank/models/account.py
@@ -7,7 +7,7 @@ account_type_choices = (
 )
 
 class Account(Model):
-    owner = ForeignKey('bank.models.customer', on_delete=CASCADE, related_name='accounts')
+    owner = ForeignKey('bank.customer', on_delete=CASCADE, related_name='accounts')
     account_name = CharField(max_length=255)
     account_type = CharField(max_length=3, choices=account_type_choices)
     created_on = DateTimeField(auto_now_add=True)
