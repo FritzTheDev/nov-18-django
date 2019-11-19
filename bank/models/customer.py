27cf2f7644d62d52ae45384f973bed83034179c3
Frederick Johnson <fritzj96@gmail.com>
2019-11-19 09:52:43 -0800

    removed branch relation from customer

--- bank/models/customer.py
+++ bank/models/customer.py
@@ -2,7 +2,5 @@ from django.db.models import Model, OneToOneField, CharField, ForeignKey, CASCAD
 
 class Customer(Model):
     base_user = OneToOneField('auth.User', on_delete=CASCADE, primary_key=True)
-    main_branch = ForeignKey('bank.models.Branch', on_delete=CASCADE, related_name='customers')
     first_name = CharField(max_length=15)
     last_name = CharField(max_length=20)
-    
