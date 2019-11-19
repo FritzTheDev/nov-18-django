20bb5f313733163ff75d4c52227f30cd84f7a2e6
Frederick Johnson <fritzj96@gmail.com>
2019-11-19 10:36:51 -0800

    added selfstring to branch.py

--- bank/models/branch.py
+++ bank/models/branch.py
@@ -4,3 +4,6 @@ from uuid import uuid4
 class Branch(Model):
     branch_uuid = CharField(default=uuid4(), max_length=36, primary_key=True)
     branch_address = CharField(max_length=100)
+
+    def __str__(self):
+        return self.branch_address
